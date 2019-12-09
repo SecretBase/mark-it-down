fc76276e892a564e748e4bbb761847c696ba0e48
SecretBase <5130999+SecretBase@users.noreply.github.com>
2019-12-09 22:16:26 +0800

    feat: enable linkify and disalbe commark strict

--- components/Preview/index.tsx
+++ components/Preview/index.tsx
@@ -1,6 +1,7 @@
 import { h, FunctionComponent } from "preact"
 import { useState, useEffect, memo } from "preact/compat"
 import { wrap } from "comlink"
+import { Spinner } from "react-bootstrap"
 
 const worker = new Worker("./markdownParser.ts")
 const fns = wrap<{ parse: parseFn }>(worker)
@@ -11,22 +12,33 @@ interface Props {
 
 type parseFn = (markdown: string) => string
 
-const useMarkdown = (markdown: string): string => {
+const useMarkdown = (
+  markdown: string
+): { html: string; isLoading: boolean } => {
   const [html, setHtml] = useState("")
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     const run = async (): Promise<void> => {
       const result = await fns.parse(markdown)
       setHtml(result)
+      setIsLoading(false)
     }
     run()
   }, [markdown])
 
-  return html
+  return { html, isLoading }
 }
 
 const Preview: FunctionComponent<Props> = ({ markdown }) => {
-  const html = useMarkdown(markdown)
+  const { html, isLoading } = useMarkdown(markdown)
+  if (isLoading)
+    return (
+      <Spinner animation="border" role="status">
+        <span className="sr-only">Loading...</span>
+      </Spinner>
+    )
+
   return <div dangerouslySetInnerHTML={{ __html: html }} />
 }
 
--- components/Preview/markdownParser.ts
+++ components/Preview/markdownParser.ts
@@ -1,7 +1,10 @@
 import MarkdownIt from "markdown-it"
 import { expose } from "comlink"
 
-const md = new MarkdownIt("commonmark")
+const md = new MarkdownIt({
+  html: true,
+  linkify: true
+})
 
 const fns = {
   parse: (markdown: string): string => {
